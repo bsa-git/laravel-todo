a2ed61ddae916c5728087056f03606f1b6b5a6d9
bsa-git <bsa2657@yandex.ru>
2016-07-12 08:59:12 +0300

    Add schedule command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
+        $schedule->command('inspire')
+                ->appendOutputTo('c:/tmp/1.txt');
         //          ->hourly();
     }
 }
